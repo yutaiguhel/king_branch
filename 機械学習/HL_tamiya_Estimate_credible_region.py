741bb4f61aee040be20726441b9d097cacbbb926
S_tamiya <44150416+tsuvihatu@users.noreply.github.com>
2018-11-13 15:44:10 +0900

    Update HL_tamiya_Estimate_credible_region.py

--- 機械学習/HL_tamiya_Estimate_credible_region.py
+++ 機械学習/HL_tamiya_Estimate_credible_region.py
@@ -5,13 +5,16 @@ Created on Tue Nov 13 12:01:15 2018
 @author: shiro
 """
 
-def Estimate_credible_region(self,level):
-    id_sorted=np.argsort(self.w,axis=0)[::-1]
-    w_sorted=np.sort(self.w,axis=0)[::-1]
-    cumsum_weights=np.cumsum(w_sorted)
-    id_cred=cumsum_weights<=level
-    
-    x_range_temp=self.x[id_sorted][id_cred]
-    x_range=np.reshape(x_range_temp,[len(x_range_temp),len(self.x[0])])
-    
-    return x_range
+    def Estimate_credible_region(self,level):
+        id_sorted=np.argsort(self.w,axis=0)[::-1]
+        w_sorted=np.sort(self.w,axis=0)[::-1]
+        cumsum_weights=np.cumsum(w_sorted)
+        id_cred=cumsum_weights<=level
+        
+        if(id_cred.all()==False):
+            x_range=self.x[id_sorted[0]]
+        else:
+            x_range_temp=self.x[id_sorted][id_cred]
+            x_range=np.reshape(x_range_temp,[len(x_range_temp),len(self.x[0])])
+        
+        return x_range
